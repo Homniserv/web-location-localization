273e5768b9297f11cf3c47eb37e203c382c0b541
moueza <mouezapeter@gmail.com>
2018-06-03 01:18:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,14 @@ console.log("lat="+lat);
 console.log("long="+long);
 
 }
-
+var latt;
+var longg;
 navigator.geolocation.getCurrentPosition(function(position) {
   do_something(position.coords.latitude, position.coords.longitude);
+  latt = position.coords.latitude;
+  longg =  position.coords.longitude;
 });
 
 document.getElementById("demo").innerHTML = "Hello JavaScript!";
+document.getElementById("lat").innerHTML = latt;
+document.getElementById("long").innerHTML = longg;
