77b37d159d06b163261962417eacde17c8a6cdb6
moueza <mouezapeter@gmail.com>
2018-06-03 01:06:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+function  do_something(lat,long){
+console.log("lat="+lat);
 
+}
+
+navigator.geolocation.getCurrentPosition(function(position) {
+  do_something(position.coords.latitude, position.coords.longitude);
+});
+
+document.getElementById("demo").innerHTML = "Hello JavaScript!";
