d46d825aa9ea7ad1a026c7664a33803d561414c5
moueza <mouezapeter@gmail.com>
2018-06-03 02:29:13 +0200

    no133

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ var latt;
 var longg;
 
 function  do_something(lat,long){
-console.log("lat no132="+lat);
+console.log("lat no133="+lat);
 console.log("long="+long);
 
  var  latt =lat;
   var longg =  long;
+ 
+ document.getElementById("lat").innerHTML = latt;
+document.getElementById("long").innerHTML = longg;
+ 
   console.log("latt="+latt);
   console.log("longg="+longg);
   
@@ -18,5 +22,4 @@ navigator.geolocation.getCurrentPosition(function(position) {
 });
 
 document.getElementById("demo").innerHTML = "Hello JavaScript!";
-document.getElementById("lat").innerHTML = latt;
-document.getElementById("long").innerHTML = longg;
+
