2afd5237b7fddf4df52119d2077674464825f6b5
moueza <mouezapeter@gmail.com>
2018-06-03 08:23:35 +0200

    #1 location manually OK

--- index.js
+++ index.js
@@ -5,20 +5,18 @@ function  do_something(lat,long){
 console.log("lat no133="+lat);
 console.log("long="+long);
 
- var  latt =lat;
-  var longg =  long;
+var  latt =lat;
+var longg =  long;
  
- document.getElementById("lat").innerHTML = latt;
+document.getElementById("lat").innerHTML = latt;
 document.getElementById("long").innerHTML = longg;
  
-  console.log("latt="+latt);
-  console.log("longg="+longg);
-  
+console.log("latt="+latt);
+console.log("longg="+longg); 
 }
 
 navigator.geolocation.getCurrentPosition(function(position) {
-  do_something(position.coords.latitude, position.coords.longitude);
- 
+  do_something(position.coords.latitude, position.coords.longitude); 
 });
 
 document.getElementById("demo").innerHTML = "Hello JavaScript!";
